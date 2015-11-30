4b279b2abb37e068aa2003037bcfca9cd007495c
radishj <423261989@qq.com>
2015-11-30 22:31:00 +0800

    10猎奇上传

---
+++ 01_10lieqi/README.md
@@ -0,0 +1,11 @@
+# 10lieqi(10张猎奇图)
+* 一款宽950PX为QQ空间设计的应用切图
+
+## 说明
+* 此为2014年在学校时练手项目
+
+## 日志
+* 2015/11/30 github初始化项目
+
+## License
+[MIT](http://opensource.org/licenses/MIT)
---
+++ 01_10lieqi/js/Global.js
@@ -0,0 +1,41 @@
+﻿function htmlScroll() {
+    var top = document.body.scrollTop || document.documentElement.scrollTop;
+    if (elFix.data_top < top) {
+        elFix.style.position = 'fixed';
+        elFix.style.top = 0;
+        elFix.style.left = elFix.data_left;
+
+        var fixFooter = document.getElementById("fixed-footer");
+        fixFooter.style.visibility = "visible";
+    }
+    else {
+        elFix.style.position = 'static';
+
+        var fixFooter = document.getElementById("fixed-footer");
+        fixFooter.style.visibility = "hidden";
+    }
+}
+
+function htmlPosition(obj) {
+    var o = obj;
+    var t = o.offsetTop;
+    var l = o.offsetLeft;
+    while (o = o.offsetParent) {
+        t += o.offsetTop;
+        l += o.offsetLeft;
+    }
+    obj.data_top = t;
+    obj.data_left = l;
+}
+
+var oldHtmlWidth = document.documentElement.offsetWidth;
+window.onresize = function () {
+    var newHtmlWidth = document.documentElement.offsetWidth;
+    if (oldHtmlWidth == newHtmlWidth) {
+        return;
+    }
+    oldHtmlWidth = newHtmlWidth;
+    elFix.style.position = 'static';
+    htmlPosition(elFix);
+    htmlScroll();
+}
