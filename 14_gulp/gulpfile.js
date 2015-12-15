017cf763814d150eb4b980a7460e698f638774e8
radishj <423261989@qq.com>
2015-12-15 15:15:53 +0800

    内容替换及版本号研究:smile:

--- 14_gulp/gulpfile.js
+++ 14_gulp/gulpfile.js
@@ -7,6 +7,10 @@ var gulp           = require('gulp'),
     minifycss      = require('gulp-minify-css'),
     livereload     = require('gulp-livereload'),
     notify         = require('gulp-notify'),
+    rename         = require('gulp-rename'),
+    // 为服务器特别定制的，快速、灵活、实施精益(lean implementation)的jQuery核心
+    cheerio        = require('gulp-cheerio'),
+    domSrc         = require('gulp-dom-src'),
     connect        = require('gulp-connect');
 
 var path = {
@@ -22,14 +26,30 @@ var path = {
 
 gulp.task('set-production', function() {
     env.production = true;
-})
+});
+
+gulp.task('replace', function() {
+    var rand = new Date().getSeconds();
+    return  gulp.src(path.dist + 'index.html')
+                .pipe(cheerio(function($) {
+                    $('script').remove();
+                    $('link').remove();
+                    $('body').append('<script src="js/index.js"></script>');
+                    $('head').append('<link rel="stylesheet" href="index.min.css?v='+ rand +'">');
+                }))
+                .pipe(rename('idx.html'))
+                .pipe(gulp.dest(path.dist));
+});
+
 // 编译less
-gulp.task('pLcss', function() {
+gulp.task('css', function() {
     if(env.production) {
         return  gulp.src(path.less + '*.less')
                     .pipe(less())
                     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+                    .pipe(gulp.dest('dist/css'))
                     .pipe(minifycss())
+                    .pipe(rename({ suffix: '.min'}))
                     .pipe(gulp.dest('dist/css'))
                     .pipe(notify({message : 'less compile complete!'}));
     } else {
@@ -50,13 +70,14 @@ gulp.task('server', function() {
     });
 });
 // 刷新
-gulp.task('reload-dev', ['pLcss'], function() {
+gulp.task('reload-dev', ['css'], function() {
     return gulp.src(path.dist + '**/*.*')
                .pipe(connect.reload())
                .pipe(notify({message : 'server reload complete!'}));
 });
 // 监听
 gulp.task('watch', function() {
+    // 其实没必要监听所有
     return gulp.watch([
                 path.src  + '**/*.*',
                 path.dist + '*.html'
@@ -64,4 +85,4 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['server', 'watch']);
-gulp.task('build', ['set-production', 'pLcss']);
+gulp.task('build', ['set-production', 'css', 'replace']);
