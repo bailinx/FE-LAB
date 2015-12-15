aa4cce650c27b6b2024ecabc1cc4d841f6d3e8bc
radishj <423261989@qq.com>
2015-12-15 13:28:50 +0800

    gulp minifycss autoprefixer

--- 14_gulp/gulpfile.js
+++ 14_gulp/gulpfile.js
@@ -1,11 +1,13 @@
 // http://www.ydcss.com/archives/18
 // http://www.cnblogs.com/xjcjcsy/p/4467751.html
 // http://segmentfault.com/a/1190000003730672
-var gulp       = require('gulp'),
-    less       = require('gulp-less'),
-    livereload = require('gulp-livereload'),
-    notify     = require('gulp-notify'),
-    connect    = require('gulp-connect');
+var gulp           = require('gulp'),
+    less           = require('gulp-less'),
+    autoprefixer   = require('gulp-autoprefixer'),
+    minifycss      = require('gulp-minify-css'),
+    livereload     = require('gulp-livereload'),
+    notify         = require('gulp-notify'),
+    connect        = require('gulp-connect');
 
 var path = {
     src      : "src/",
@@ -14,14 +16,31 @@ var path = {
     less     : "src/less/",
     img      : "src/images/",
     dist     : "dist/"
-}
+}, env = {
+    production: false
+};
+
+gulp.task('set-production', function() {
+    env.production = true;
+})
 // 编译less
-gulp.task('less', function() {
-    gulp.src(path.less + 'index.less')
-        .pipe(less())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(notify({message : 'less compile complete!'}));
+gulp.task('pLcss', function() {
+    if(env.production) {
+        return  gulp.src(path.less + '*.less')
+                    .pipe(less())
+                    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+                    .pipe(minifycss())
+                    .pipe(gulp.dest('dist/css'))
+                    .pipe(notify({message : 'less compile complete!'}));
+    } else {
+        return  gulp.src(path.less + '*.less')
+                    .pipe(less())
+                    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+                    .pipe(gulp.dest('dist/css'))
+                    .pipe(notify({message : 'less compile complete!'}));
+    }
 });
+
 // server
 gulp.task('server', function() {
     connect.server({
@@ -31,18 +50,18 @@ gulp.task('server', function() {
     });
 });
 // 刷新
-gulp.task('reload-dev', ['less'], function() {
-    gulp.src(path.dist + '**/*.*')
-        .pipe(connect.reload())
-        .pipe(notify({message : 'server reload complete!'}));
+gulp.task('reload-dev', ['pLcss'], function() {
+    return gulp.src(path.dist + '**/*.*')
+               .pipe(connect.reload())
+               .pipe(notify({message : 'server reload complete!'}));
 });
 // 监听
 gulp.task('watch', function() {
-    gulp.watch([
-        path.src  + '**/*.*',
-        path.dist + '*.html'
-    ], ['reload-dev']);
+    return gulp.watch([
+                path.src  + '**/*.*',
+                path.dist + '*.html'
+            ], ['reload-dev']);
 });
 
 gulp.task('default', ['server', 'watch']);
-gulp.task('build', ['less']);
+gulp.task('build', ['set-production', 'pLcss']);
