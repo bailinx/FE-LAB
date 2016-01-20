a572377dc075e637aae2adfa849175b422dcbbd7
radishj <423261989@qq.com>
2016-01-20 23:05:39 +0800

    sass study

---
+++ 15_sass/Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    require('load-grunt-tasks')(grunt);
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            options: {
+                protocol: 'http',
+                port: 9000, // 静态服务器端口
+                hostname: '*',
+                livereload: 35729 // watch监听的端口
+            },
+            server: {
+                options: {
+                    open: true,
+                    base: [
+                        'dist'
+                    ]
+                }
+            }
+        },
+        watch: {
+            sass: {
+                options: {
+                    livereload: '<%=connect.options.livereload%>'
+                },
+                files: [
+                    "sass/*.scss",
+                    "dist/**/*.html"
+                ],
+                tasks: ['sass']
+            }
+            /*scripts: {
+                files: "js/*.js",
+                tasks: ['uglify']
+            },
+            css: {
+                files: "css/*.css",
+                tasks: ['cssmin']
+            }*/
+        },
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'sass/',
+                    src: ['*.scss'],
+                    dest: 'dist/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+        less: {
+            compile: {
+                options: {
+                    strictMath: true,
+                    sourceMap: true, // 多文件不需要后面配置
+                    // outputSourceFiles: true,
+                    // sourceMapURL: '<%= pkg.name %>.css.map',
+                    // sourceMapFilename: 'dist/css/<%= pkg.name %>.css.map'
+                },
+                expand: true, // 动态扩展
+                cwd: 'less/', // less源文件夹
+                // src: ['**/*.less','!import.less'],
+                src: ['*.less','!import.less'], // 不需要遍历子文件夹
+                dest: 'dist/css/',
+                ext: '.css'
+                // {
+                //     "dist/css/<%= pkg.name %>.css": "less/start.less"
+                // }
+            }
+        }
+    });
+
+    // grunt.loadNpmTasks('grunt-contrib-livereload');
+    // grunt.loadNpmTasks('grunt-contrib-less');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('serve', [
+        'connect:server',
+        'watch'
+    ]);
+    // grunt.registerTask('default', ['watch']);
+    grunt.registerTask('sassc', ['sass']);
+};
