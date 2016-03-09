6f0c5f5407651be4493a345aeb1f63c60efc0325
radishj <423261989@qq.com>
2016-03-09 23:33:50 +0800

    angular promise complete

--- 20_angular/04_Angular-Promise/js/controller.js
+++ 20_angular/04_Angular-Promise/js/controller.js
@@ -1,12 +1,14 @@
 angular.module("MyController", [])
     .controller("IndexController", ["$scope", "githubService", function($scope, githubService) {
-        $scope.name = "dreamapple";
+        $scope.name = "angular repos";
         $scope.show = true;
+        //then(successFunc, errorFunc, notifyFunc)
         githubService.getPullRequests().then(function(result) {
             $scope.data = result;
         }, function(error) {
             $scope.data = "error!";
         }, function(progress) {
+            // 进度
             $scope.progress = progress;
             $scope.show = false;
         });
--- 20_angular/04_Angular-Promise/js/service.js
+++ 20_angular/04_Angular-Promise/js/service.js
@@ -1,26 +1,32 @@
 angular.module("MyService", [])
-    .factory('githubService', ["$q", "$http", function($q, $http){
-        var getPullRequests = function(){
-        var deferred = $q.defer();
-        var promise = deferred.promise;
-        var progress;
-        $http.get("https://api.github.com/repos/angular/angular.js/pulls")
-        .success(function(data){
-            var result = [];
-            for(var i = 0; i < data.length; i++){
-                result.push(data[i].user);
-                progress = (i+1)/data.length * 100;
-                deferred.notify(progress);
-            }
-            deferred.resolve(result);
-            })
-        .error(function(error){
-            deferred.reject(error);
-        });
-        return promise;
-    }
+    .factory('githubService', ["$q", "$http", function($q, $http) {
+        var getPullRequests = function() {
+            // 通过$q实例化defer对象
+            var deferred = $q.defer();
+            // 获取deferred对象的promise属性
+            var promise = deferred.promise;
+            var progress;
+            $http.get("https://api.github.com/repos/angular/angular.js/pulls")
+                .success(function(data) {
+                    var result = [];
+                    for (var i = 0; i < data.length; i++) {
+                        result.push(data[i].user);
+                        progress = (i + 1) / data.length * 100;
+                        // 处理进度
+                        deferred.notify(progress);
+                    }
+                    // 成功获取到结果
+                    deferred.resolve(result);
+                })
+                .error(function(error) {
+                    // 失败
+                    deferred.reject(error);
+                });
+            // 返回promise
+            return promise;
+        }
 
-    return {
-        getPullRequests: getPullRequests
-    };
-}]);
+        return {
+            getPullRequests: getPullRequests
+        };
+    }]);
