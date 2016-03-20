d7b872fafa1a26ea8e8be8f3f17870db96c03dc9
radishj <423261989@qq.com>
2016-03-20 23:02:11 +0800

    react hot loader

--- 16_webpack/js/index.jsx
+++ 16_webpack/js/index.jsx
@@ -3,11 +3,12 @@ var Hello = require('./hello');
 
 module.exports = React.createClass({
     render: function () {
-        var names = ['smith', 'baili', 'radishj'];
+        var names = ['smith', 'baili', 'radishj', 'test react hot loader'];
         var nodes = [];
         names.map(function(name) {
             nodes.push(<Hello key={name} name={name}/>)
         });
+
         return (
             <div>{nodes}</div>
         );
--- 16_webpack/webpack.config.js
+++ 16_webpack/webpack.config.js
@@ -1,12 +1,16 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './js/app.jsx'
-    },
+    entry: [
+        'webpack-dev-server/client?http://127.0.0.1:3000',
+        'webpack/hot/only-dev-server',
+        './js/app.jsx'
+    ],
     output: {
         path: __dirname + '/assets',
-        filename: '[name].bundle.js'
+        // 这里如果不用url，则会出现随机的文件名，导致引用不正确
+        publicPath: 'http://localhost:3000/assets',
+        filename: 'bundle.js'
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
@@ -17,9 +21,22 @@ module.exports = {
                 loader: 'babel-loader'
             }, {
                 test: /\.jsx$/,
-                loader: 'babel-loader!jsx-loader?harmony'
+                loader: 'react-hot!babel-loader!jsx-loader?harmony'
+            }, {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }, {
+                test: /\.(css)$/,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
             }
 
         ]
-    }
+    },
+    plugins: [
+        // 一定要加这个插件
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
